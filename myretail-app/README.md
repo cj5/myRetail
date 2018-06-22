a62227a98d4cbef772e2abcc10cc43913b8cbbd3
Cortney Rasmussen <cortney@opticpop.com>
2018-06-22 17:08:13 -0500

    Initial project setup

---
+++ myretail-app/README.md
@@ -0,0 +1,6 @@
+
+## Project Setup
+
+In the myretail-app directory, run:
+
+### `yarn start`
---
+++ myretail-app/src/js/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 className="h1">myRetail Initial Beginnings 🎉</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
