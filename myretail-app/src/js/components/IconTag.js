9cc6fe9c8d61d2f8b25a6b02abe3422476b5a132
Cortney Rasmussen <cortney@opticpop.com>
2018-07-02 10:00:51 -0500

    All image assets not provided in data

---
+++ myretail-app/src/js/components/IconTag.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import tag from './assets/images/icon-tag.png'
+
+export default class IconTag extends Component {
+  render() {
+    return(
+      <img src={tag} alt="" />
+    )
+  }
+}
